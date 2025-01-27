1bd4e0aae17093e2e12833166fd4dae83e662d05
SzaboTamasKeri <szabo.tamas-2020+@keri.mako.hu>
2025-01-27 22:24:29 +0100

    calendar

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,67 @@
+<!-- JS -->
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  // Moment.js import
+  import moment from "moment";
+
+  // Calendar data
+  let data = {
+		year: moment().year(),				
+		month: moment().month(),
+		day: moment().date(),
+		dayNumberOfWeek: moment().day(),	
+		startOfMonthNumberOfWeek: moment().startOf('month').day(),	
+		daysInMonth: moment().daysInMonth(),
+		daysInMonthPrev: moment().subtract( 1, 'months').daysInMonth(),
+		daysInMonthNext: moment().add( 1, 'months').daysInMonth()
+	};
+	data.weeksInMonth = Math.ceil((data.daysInMonth + data.startOfMonthNumberOfWeek - 1) / 7);
+
+  console.log(data.daysInMonthPrev)
+
 </script>
 
+<!-- HTML -->
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <div class="container">
+		<div class="row mt-5 justify-content-center">
+			<h3 class="text-center">{moment().format('YYYY-MM-DD')}</h3>
+		</div>
+		<div class="row justify-content-center mt-3">
+			
+			<table class="table table-bordered w-auto">
+				<thead>
+					<tr>
+            {#each ['H', 'K', 'Sz', 'Cs', 'P', 'Szo', 'V'] as nap}
+						  <th class="text-center">{nap}</th>
+            {/each}
+					</tr>
+				</thead>
+				<tbody>
+          {#each Array(data.weeksInMonth) as _, w}
+					  <tr class="text-center">
+              {#each Array(7) as _, i}
+                {@const day = w * 7 + i - data.startOfMonthNumberOfWeek + 2}
+                <td class:text-muted={day < 1 || day > data.daysInMonth} 
+                    class:fw-bold={day > 0 && day <= data.daysInMonth} 
+                    class:text-primary={day == data.day}>
+                  {#if day < 1}
+                    {data.daysInMonthPrev + day}
+                  {:else if day > data.daysInMonth}
+                    {day - data.daysInMonth}
+                  {:else}  
+                    {day}
+                  {/if}
+                </td>
+              {/each}
+					  </tr>
+          {/each}
+				</tbody>
+			</table>
+		</div>
+	</div>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<!-- CSS
+  <style></style>
+-->
